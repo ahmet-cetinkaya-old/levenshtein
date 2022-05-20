55298b27d0d1fc003bfd9f81f6fba4ac05c85c60
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-05-20 10:44:12 +0300

    initial

---
+++ Levenshtein/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Levenshtein
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringDistance stringDistance = new StringDistance(sourceInput.Text, targetInput.Text);
+            Tuple<int, int[,]> result = stringDistance.levenshtein();
+            outputDistance.Content = result.Item1;
+            outputMatrix.Children.Clear();
+            FillTable(result.Item2);
+        }
+
+        private void FillTable(int[,] matrix)
+        {
+            Button[,] cells = new Button[matrix.GetLength(0) + 2, matrix.GetLength(1) + 2];
+
+            int top = 0;
+            int left = 0;
+
+            for (int i = 0; i < cells.GetUpperBound(0); i++) // satır
+            {
+                for (int j = 0; j < cells.GetUpperBound(1); j++) // sütun
+                {
+                    cells[i, j] = new Button();
+                    cells[i, j].Height = 40;
+                    cells[i, j].Width = 40;
+                    cells[i, j].Margin = new Thickness(left, top, -left, -top);
+                    cells[i, j].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                    cells[i, j].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+
+                    if (i == 0 && j == 0) cells[i, j].Background = new SolidColorBrush(Colors.White);
+                    else if (i == 1 || j == 1) cells[i, j].Background = new SolidColorBrush(Colors.LightGreen);
+                    else if (i == cells.GetUpperBound(0) - 1 && j == cells.GetUpperBound(1) - 1) cells[i, j].Background = new SolidColorBrush(Colors.IndianRed);
+                    else if ((i + j) % 2 == 0) cells[i, j].Background = new SolidColorBrush(Colors.LightGray);
+
+                    if ((i == 0 && j < 2) || (j == 0 && i < 2)) cells[i, j].Content = "";
+                    else if (j == 0 && i > 1) cells[i, j].Content = sourceInput.Text[i - 2];
+                    else if (i == 0 && j > 1) cells[i, j].Content = targetInput.Text[j - 2];
+                    else if (i > 0 && j > 0) cells[i, j].Content = matrix[i - 1, j - 1];
+
+                    outputMatrix.Children.Add(cells[i, j]);
+                    left += 40;
+                }
+                top += 40;
+                if (i == 0) outputMatrix.Width = left;
+                left = 0;
+            }
+            outputMatrix.Height = top;
+        }
+    }
+}
---
+++ Levenshtein/StringDistance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levenshtein
+{
+    internal class StringDistance
+    {
+        private string source;
+        private string target;
+
+        public StringDistance(string source, string target)
+        {
+            this.source = source;
+            this.target = target;
+        }
+
+        public Tuple<int, int[,]> levenshtein()
+        {
+            int s = this.source.Length;
+            int t = this.target.Length;
+
+            // Kelime boyları ile hesaplama matrisi oluşturulur. Boşluk karakteri ile de yapılacak karşılaştırmadan bir fazlası kullanılmıştır.
+            int[,] matrix = new int[s + 1, t + 1];
+
+            // Eğer değerlerinden biri boş ise diğerinin uzunluğu kadar maliyet çıkacaktır. Bunların kontrolü yaparak fonksiyon bitirilebilir.
+            if (s == 0) return Tuple.Create(t, matrix);
+            if (t == 0) return Tuple.Create(s, matrix);
+
+            // Yatay ve düşey eksenlerdeki standart boşluk elemanları doldurulur.
+            for (int i = 0; i <= s; i++) matrix[i, 0] = i;
+            for (int j = 0; j <= t; j++) matrix[0, j] = j;
+
+            // Harfleri tek tek gezerek karşılaştırma işlemleri yapılır. Sonuca göre ilgili matriks elemanına değer atanır.
+            for (int i = 1; i <= s; i++)
+            {
+                for (int j = 1; j <= t; j++)
+                {
+                    if (this.source[i - 1] == this.target[j - 1]) matrix[i, j] = matrix[i - 1, j - 1]; // Harflerin eşit olması durumunda sol üst çaprazındaki eleman değiştirilmeden atanır.
+                    else matrix[i, j] = Math.Min(Math.Min(matrix[i - 1, j], matrix[i, j - 1]), matrix[i - 1, j - 1]) + 1; // Farklı olması durumunda önceki değerlere bir eklenerek değeri en küçük olan atanır.
+                }
+            }
+
+            return Tuple.Create(matrix[s, t], matrix);
+        }
+    }
+}
